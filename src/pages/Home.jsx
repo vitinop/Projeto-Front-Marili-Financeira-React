389c21262555e3713266f5912c00fe3509ce287c
Matheus Emanoel <matheusemanoel1407@gmail.com>
2022-10-04 18:00:03 -0300

    home0.0.9

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,11 @@
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Figure } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
+import Carousel from 'react-bootstrap/Carousel';
 
 import Nav from "react-bootstrap/Nav";
-import Car from "../assets/icons/car.png";
-import House from "../assets/icons/house.png";
-import SeguroDeVida from "../assets/icons/qualidade-de-vida.png";
-import Contrato from "../assets/icons/acordo.png";
-import Doutor from "../assets/icons/doutor.png";
-import Partner from "../assets/img/partner.png";
-import Favourite from "../assets/icons/favourite.png";
-import Chat from "../assets/icons/chat.png";
-import Configuracoes from "../assets/icons/configuracoes.png";
+import Familia from '../assets/img/familia_feliz_1.jpg'
+import Carro from '../assets/img/carro-bolha.png'
 
-import CarroBolha from "../assets/img/carro-bolha.png";
 import "./Home.css";
 import "../App.css";
 
@@ -20,37 +13,86 @@ export default function Home() {
   return (
     <>
       <Container>
-        <section className="mt-5 d-flex justify-content-center">
-          <div className="d-flex flex-column w-50">
+        <section className=" mt-5 d-flex justify-content-center">
+          <div className="whySustainability p-1 d-flex flex-column w-50">
             <h1 className="text-banner">
-              ​O que é Sustentabilidade Financeira?
+              O que é Sustentabilidade Financeira?
             </h1>
             <h3>Bem vindo (a)!</h3>
-            <h2 className="text-break">
+            <p className="text-break">
               O valor da sustentabilidade financeira se traduz no uso dos
               recursos de forma equilibrada. Isso significa objetivar a garantia
               da segurança financeira do seu patrimônio por meio do uso
               eficiente dos recursos que temos, sempre com uma visão de longo
               prazo.
-            </h2>
+            </p>
           </div>
         </section>
       </Container>
 
-      <Container>
-        <section className="mt-5 d-flex justify-content-center">
-          <div className="d-flex flex-column w-50">
-            <h1 className="text-banner">Consultoria com Propósito</h1>
+      <Container fluid className="p-0 mt-5">
+        <section className="purposeSection mt-5 d-flex justify-content-center">
+          <div className="purposeText d-flex row flex-wrap justify-content-center">
+            <h3 className="p-1">​Consultoria com Propósito</h3>
+            <p className="p-2 w-85">​A MR surgiu da necessidade de proteger a segurança patrimonial dos nossos clientes.<br />
 
-            <h2 className="text-break">
-              A MR surgiu da necessidade de proteger a segurança patrimonial dos
-              nossos clientes. Atuamos com atendimento personalizado, traçando
-              um planejamento estratégico de sustentabilidade financeira para o
-              seu patrimônio. Ajudamos nossos clientes a viabilizar seus
-              objetivos e transformar seus desafios em tranquilidade.
-            </h2>
+              Atuamos com atendimento personalizado, traçando um planejamento estratégico de sustentabilidade financeira para o seu patrimônio.<br />
+
+              Ajudamos nossos clientes a viabilizar seus objetivos e transformar seus desafios em tranquilidade.</p>
+
+
+            <Carousel className="purposeImg">
+              <Carousel.Item interval={1000}>
+                <img
+                  className="d-block w-100"
+                  src={Familia}
+                  alt="First slide"
+                />
+
+              </Carousel.Item>
+              <Carousel.Item interval={500}>
+                <img
+                  className="d-block w-100"
+                  src={Familia}
+                  alt="Second slide"
+                />
+
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={Familia}
+                  alt="Third slide"
+                />
+
+              </Carousel.Item>
+
+              <Carousel.Item interval={200}>
+                <img
+                  className="d-block w-100"
+                  src={Familia}
+                  alt="Second slide"
+                />
+              </Carousel.Item>
+            </Carousel>
+
           </div>
         </section>
+      </Container>
+
+      <Container className="mt-5">
+        <section>
+          <div className="imgCardContainer d-flex">
+          <img src={Carro} alt=""/>
+           
+       
+          <div className="imgCardContainerText">
+          <p>Lorem Ipsum</p>
+          </div>
+            
+          </div>
+        </section>
+
       </Container>
     </>
   );
