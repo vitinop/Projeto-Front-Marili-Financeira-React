2ecc51898a39eb92803066243cea70a01dd28c1b
Matheus Emanoel <matheusemanoel1407@gmail.com>
2022-10-07 19:34:24 -0300

    mosaico0.1

--- src/pages/ComoFunciona/ComoFunciona.jsx
+++ src/pages/ComoFunciona/ComoFunciona.jsx
@@ -5,27 +5,35 @@ import Contrato from '../../assets/img/contrato.jpg'
 import '../../App.css'
 import './ComoFunciona.css'
 
-export default function ComoFunciona(){
+export default function ComoFunciona() {
   return (
-      <>
+    <>
       <Container fluid className='p-0 comoFunciona'>
         <div className='comoFuncionaContainer'>
           <div className='comoFuncionaTitle'>
             <h3>Como funciona:</h3>
           </div>
-          <div className='comoFuncionaTextImg d-flex align-items-center '>
-            <img src={Contrato} alt="" />
-            <p className='m-5'>​Identificamos os seus desafios e os seus objetivos</p>
+          <div></div>
+          <div className='d-flex align-items-center flex-row flex-wrap '>
+            <div className='comoFuncionaImg' style={{ backgroundImage: `url(${Contrato})` }}>
+            </div>
+            <div className='comoFuncionaText'>
+              <p>​Identificamos os seus desafios e os seus objetivos</p>
+            </div>
+          </div>
+          <div className='d-flex align-items-center flex-row-reverse flex-wrap '>
+            <div className='comoFuncionaImg' style={{ backgroundImage: `url(${Contrato})` }}>
+            </div>
+            <div className='comoFuncionaText'>
+              <p>​Identificamos os seus desafios e os seus objetivos</p>
+            </div>
           </div>
 
-          <div className='comoFuncionaTextImg d-flex flex-row-reverse align-items-center '>
-            <img src={Contrato} alt="" />
-            <p className='m-5'>​Analisamos o seu patrimônio</p>
-          </div>
+
 
 
         </div>
       </Container>
-      </>
+    </>
   )
 }
