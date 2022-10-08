37948675c14f60ad4568032920db6a80fa1ded9c
Victor Luz <85527775+vitinop@users.noreply.github.com>
2022-10-08 03:09:45 -0300

    Update ComoFunciona.jsx

--- src/pages/ComoFunciona/ComoFunciona.jsx
+++ src/pages/ComoFunciona/ComoFunciona.jsx
@@ -8,20 +8,20 @@ import './ComoFunciona.css'
 export default function ComoFunciona() {
   return (
     <>
-      <Container fluid className='p-0 comoFunciona'>
+      <Container fluid className='p-0 d-flex flex-column comoFunciona'>
         <div className='comoFuncionaContainer'>
           <div className='comoFuncionaTitle'>
             <h3>Como funciona:</h3>
           </div>
           <div></div>
-          <div className='d-flex align-items-center flex-row flex-wrap '>
+          <div className='d-flex align-items-center justify-content-center flex-row flex-wrap flex-lg-nowrap '>
             <div className='comoFuncionaImg' style={{ backgroundImage: `url(${Contrato})` }}>
             </div>
             <div className='comoFuncionaText'>
               <p>​Identificamos os seus desafios e os seus objetivos</p>
             </div>
           </div>
-          <div className='d-flex align-items-center flex-row-reverse flex-wrap '>
+          <div className='d-flex align-items-center justify-content-center flex-row-reverse flex-wrap flex-lg-nowrap'>
             <div className='comoFuncionaImg' style={{ backgroundImage: `url(${Contrato})` }}>
             </div>
             <div className='comoFuncionaText'>
